250674b6c41619a6735103d7d984d530e6f92b6c
adrianopiacentini <adrianopiacentini03@gmail.com>
2025-01-07 17:16:50 +0100

    Esercizio completato, grazie Loris!!

--- blog-api-crud/app.js
+++ blog-api-crud/app.js
@@ -3,10 +3,20 @@ const app = express()
 const port = 3000
 const posts = require('./data/posts.js')
 const router = require('./routers/posts.js')
+const cors = require("cors")
 const handleError = require('./middlewares/handleError.js')
 app.use(express.json());
 app.use(express.static('public'))
-app.use('/post', router)
+
+app.use(
+    cors({
+        origin: "http://localhost:5173",
+    })
+)
+
+app.use('/post', router)    
+
+
 
 app.get('/', (req, res) => {
     res.send('Server del mio blog di cucina')
--- react-form/src/App.jsx
+++ react-form/src/App.jsx
@@ -1,36 +1,73 @@
-import { useState } from 'react'
-import './App.css'
-import posts from '../data/posts'
+import { useState, useEffect } from 'react';
+import './App.css';
+import posts from '../data/posts'; 
+import axios from 'axios'; 
+
+const api = "http://localhost:3000";
+
+const initialFormData = {
+  id: "",
+  title: "",
+};
 
 function App() {
-  const [post, setPost] = useState(posts)
-  const [newPost, setnewPost] = useState('')
+  const [post, setPost] = useState([]);
+  const [newPost, setNewPost] = useState(initialFormData);
 
-  const createPost = event => {
-    event.preventDefault()
-    setPost([...post, { id: Date.now(), title: newPost }])
-    setnewPost('')
-  }
+  useEffect(() => {
+    getPosts();
+  }, []);
+
+  const getPosts = () => {
+    axios.get(`${api}/post`).then((resp) => {
+      console.log(resp);
+      setPost(resp.data.data);
+    });
+  };
+
+  const createPost = (event) => {
+    event.preventDefault();
+    setPost([...post, { id: Date.now(), title: newPost.title }]);
+    setNewPost(initialFormData); 
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setNewPost((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
 
   const deletePost = (postId) => {
-    setPost(post.filter(post => post.id !== postId))
-  }
+    setPost(post.filter((post) => post.id !== postId));
+  };
 
   return (
     <>
       <div>
         <form onSubmit={createPost}>
-          <label htmlFor='desc'>Contenuto</label>
-          <input type='text' name='desc' id='desc' value={newPost} onChange={test => { setnewPost(test.target.value) }} />
-          <button type='submit'>Invia</button>
+          <label htmlFor="title">Contenuto</label>
+          <input
+            type="text"
+            name="title"
+            id="id"
+            value={newPost.title}
+            onChange={handleChange}
+          />
+          <button type="submit">Invia</button>
         </form>
       </div>
-      {(post.length !== 0) ? post.map(curPost => <div key={curPost.id}>
-        {curPost.title}
-        <button onClick={() => deletePost(curPost.id)}>Cancella</button>
-      </div>) : <p>La pagina è vuota</p>}
+      {post.length !== 0
+        ? post.map((curPost) => (
+            <div key={curPost.id}>
+              {curPost.title}
+              <button onClick={() => deletePost(curPost.id)}>Cancella</button>
+            </div>
+          ))
+        : <p>La pagina è vuota</p>}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
