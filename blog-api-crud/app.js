ac28c9395d6785f6c0b8ab725cb0930cf1af96db
adrianopiacentini <adrianopiacentini03@gmail.com>
2025-01-07 13:08:52 +0100

    first commit

---
+++ blog-api-crud/app.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const posts = require('./data/posts.js')
+const router = require('./routers/posts.js')
+const handleError = require('./middlewares/handleError.js')
+app.use(express.json());
+app.use(express.static('public'))
+app.use('/post', router)
+
+app.get('/', (req, res) => {
+    res.send('Server del mio blog di cucina')
+})
+
+app.get ('/bacheca', (req, res) => {
+    res.json({
+        postsList: posts,
+        postsNum: posts.length
+    })
+})
+
+app.use(handleError)
+
+app.listen(port, () => {})
